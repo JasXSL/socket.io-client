3e99a4c962178d3615e1587b116152b32c47ec06
Kadah <Kadah.Coba@gmail.com>
2019-08-03 17:42:11 -0700

    Renamed PORT_DEFAULT, conflicts with my sketch

--- SocketIoClient.h
+++ SocketIoClient.h
@@ -22,9 +22,9 @@
 
 //#define SOCKETIOCLIENT_USE_SSL
 #ifdef SOCKETIOCLIENT_USE_SSL
-	#define DEFAULT_PORT 443
+	#define SIO_PORT_DEFAULT 443
 #else
-	#define DEFAULT_PORT 80
+	#define SIO_PORT_DEFAULT 80
 #endif
 #define DEFAULT_URL "/socket.io/?transport=websocket"
 
@@ -40,8 +40,8 @@ private:
 	void webSocketEvent(WStype_t type, uint8_t * payload, size_t length);
     void initialize();
 public:
-    void beginSSL(const char* host, const int port = DEFAULT_PORT, const char* url = DEFAULT_URL, const char* CA_cert = NULL);
-	void begin(const char* host, const int port = DEFAULT_PORT, const char* url = DEFAULT_URL);
+    void beginSSL(const char* host, const int port = SIO_PORT_DEFAULT, const char* url = DEFAULT_URL, const char* CA_cert = NULL);
+	void begin(const char* host, const int port = SIO_PORT_DEFAULT, const char* url = DEFAULT_URL);
 	void loop();
 	void on(const char* event, std::function<void (const char * payload, size_t length)>);
 	void emit(const char* event, const char * payload = NULL);
